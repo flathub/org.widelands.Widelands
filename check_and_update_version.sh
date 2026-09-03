2b15ee728146fcdd2e648417134638addca80509
Tóth András <80712255+tothxa@users.noreply.github.com>
2026-09-03 02:12:46 +0200

    update to git 9eda74f (#89)
     - fix version editing of metainfo.xml
     - update runtime to 26.08 final
     - update submodules

--- check_and_update_version.sh
+++ check_and_update_version.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# This is a script to mangle xdg/org.widelands.Widelands.metainfo.xml for beta versions
+# of the Widelands flatpak package.
+#
+# To fix up the latest release tag, we need a release date and a version.
+# There are 2 possible cases:
+#
+#  - Development version identified by a git commit:
+#      The version string should be the output from utils/detect_revision.py, but that
+#      requires the full git history to work, however, flatpak-builder tries to do a
+#      shallow clone whenever it can. As a work-around, we store the version string in
+#      the manifest and pass it in the WL_VERSION environment variable.
+#      The date can still be taken from git.
+#
+#  - RC version built from source tarball:
+#      The version string is stored in the WL_RELEASE file, but the date has to be set
+#      in the manifest and passed in the WL_RELEASE_DATE environment variable.
+#
+# We identify the case by checking which variable is set.
+
 SANITY_WARNING="Exactly one of WL_VERSION or WL_RELEASE_DATE must be set."
 
 if [ -z "$WL_RELEASE_DATE" ]
@@ -52,14 +71,6 @@ WL_RELEASE_STRING+='">'
 # Modify xdg/org.widelands.Widelands.metainfo.xml:
 #  * Set version
 #  * Remove the binary from provides, because flatpak doesn't export it
-sed -e 's/<release.* version="1.3~git">'/"${WL_RELEASE_STRING}"/ \
+sed -e 's/<release.* version="1.[0-9]*~git">'/"${WL_RELEASE_STRING}"/ \
     -e "/<binary>/d" \
     -i xdg/org.widelands.Widelands.metainfo.xml
-
-# Validate metainfo.xml with the changes
-if which appstreamcli >/dev/null
-then
-  appstreamcli validate --no-net xdg/org.widelands.Widelands.metainfo.xml
-else
-  echo 'Cannot validate modified metainfo.xml, appstreamcli not found'
-fi
